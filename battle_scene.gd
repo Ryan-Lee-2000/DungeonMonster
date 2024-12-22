0987fea612121fc7b3773e11e17f37e07ff21c89
Ryan-Lee-2000 <96522036+Ryan-Lee-2000@users.noreply.github.com>
2024-12-23 01:22:45 +0800

    Added textbox functionality to Battle Scene
    Added the scrolling textbox feature for the Battle Scene to allow for scrolling text describing what is happening in the battle.

--- battle_scene.gd
+++ battle_scene.gd
@@ -1,8 +1,12 @@
 extends CanvasLayer
 
+const TextDisplay = preload("res://text_line.tscn")
+
 var battle_start = false
+@onready var screen = $MarginContainer/PanelContainer/VBoxContainer/PanelContainer/ScrollContainer/MarginContainer/VBoxContainer
+@onready var scroll = $MarginContainer/PanelContainer/VBoxContainer/PanelContainer/ScrollContainer
 
-
+var lineLimit = 20 #number of lines allowed to be displayed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta) -> void:
@@ -14,5 +18,16 @@ func _process(_delta) -> void:
 	pass
 
 func preparefield():
-	
+	for x in range(0,30):
+		showText("Hey what's up lmao")
 	pass
+
+func showText(textToShow):
+	var text = TextDisplay.instantiate()
+	text.set_text(textToShow)
+	screen.add_child(text)
+
+func cleaningDuty():
+	var childCount = screen.get_children()
+	if(childCount.size() > lineLimit):
+		screen.get_child(0).free()
